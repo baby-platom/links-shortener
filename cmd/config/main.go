b03b75c195e8cd925bc7e56eb91d64af65b095e3
baby-platom <platon200502@gmail.com>
2023-06-12 18:14:06 +0600

    use envs

--- cmd/config/main.go
+++ cmd/config/main.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 // Config includes variables parsed from flags
@@ -15,4 +16,11 @@ func ParseFlags() {
 	flag.StringVar(&Config.Address, "a", ":8080", "address and port to run server")
 	flag.StringVar(&Config.BaseAddress, "b", "http://localhost:8080", "base address for shortened URLs")
 	flag.Parse()
+
+	if envAddress := os.Getenv("SERVER_ADDRESS"); envAddress != "" {
+		Config.Address = envAddress
+	}
+	if envBaseAddress := os.Getenv("BASE_URL"); envBaseAddress != "" {
+		Config.BaseAddress = envBaseAddress
+	}
 }

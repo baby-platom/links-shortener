79d0d78282f59d8209bde66c3795be712e9c02bd
baby-platom <platon200502@gmail.com>
2023-06-18 12:47:45 +0600

    suggestions implemented

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,66 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-
-	"github.com/baby-platom/links-shortener/cmd/config"
-	"github.com/baby-platom/links-shortener/internal/shortid"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/baby-platom/links-shortener/internal/app"
+	"github.com/baby-platom/links-shortener/internal/config"
 )
-
-type customHandler struct{}
-
-var shortenedUrlsByID = make(map[string]string)
 
 func main() {
 	config.ParseFlags()
 
-	if err := run(); err != nil {
+	if err := app.Run(); err != nil {
 		panic(err)
 	}
-}
-
-func run() error {
-	return http.ListenAndServe(config.Config.Address, Router())
-}
-
-// Router prepares and returns chi.Router
-func Router() chi.Router {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-
-	r.Post("/", ShortenURLHandler)
-	r.Get("/{id}", RestoreURLHandler)
-	return r
-}
-
-// ShortenURLHandler returns a shortened version of a passed URL
-func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	id := shortid.GenerateShortID()
-	shortenedUrlsByID[id] = string(body)
-	fmt.Printf("test/n%s/n/ntest", string(body))
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "%s/%s", config.Config.BaseAddress, id)
-}
-
-// RestoreURLHandler restore a URL if it before was shortened
-func RestoreURLHandler(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	url, ok := shortenedUrlsByID[id]
-	if !ok {
-		http.Error(w, "Nonexistent Id", http.StatusBadRequest)
-		return
-	}
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// "fmt"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -10,11 +11,12 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/baby-platom/links-shortener/internal/app"
 )
 
 var testingURL = "https://music.yandex.kz/home"
-var shortenedURL string
-var ts = httptest.NewServer(Router())
+var ts = httptest.NewServer(app.Router())
 
 type header struct {
 	name  string
@@ -25,14 +27,23 @@ type want struct {
 	contentType string
 	headers     []header
 }
+type request struct {
+	method string
+	path   string
+	body   io.Reader
+}
 type test struct {
-	name string
-	want want
+	name    string
+	request request
+	want    want
 }
 
-func testRequest(t *testing.T, ts *httptest.Server, method,
-	path string, body io.Reader) (*http.Response, string) {
-	req, err := http.NewRequest(method, ts.URL+path, body)
+func testRequest(
+	t *testing.T,
+	ts *httptest.Server,
+	request request,
+) (*http.Response, string) {
+	req, err := http.NewRequest(request.method, ts.URL+request.path, request.body)
 	require.NoError(t, err)
 
 	client := ts.Client()
@@ -51,49 +62,91 @@ func testRequest(t *testing.T, ts *httptest.Server, method,
 func TestShortenURLHandler(t *testing.T) {
 	tests := []test{
 		{
-			name: "positive test #1",
+			name: "positive test #0",
+			request: request{
+				method: http.MethodPost,
+				path:   "/",
+				body:   strings.NewReader(testingURL),
+			},
 			want: want{
 				code:        http.StatusCreated,
 				contentType: "text/plain",
 			},
 		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			res, resBodyString := testRequest(t, ts, http.MethodPost, "/", strings.NewReader(testingURL))
-			res.Body.Close()
-
-			assert.Equal(t, test.want.code, res.StatusCode)
-			assert.NotEmpty(t, resBodyString)
-			assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
-
-			resBodySlice := strings.Split(resBodyString, "/")
-			shortenedURL = resBodySlice[len(resBodySlice)-1]
-		})
-	}
-}
-
-func TestRestoreURLHandler(t *testing.T) {
-	tests := []test{
 		{
-			name: "positive test #1",
+			name: "negative test #0",
+			request: request{
+				method: http.MethodPost,
+				path:   "/",
+				body:   strings.NewReader("  "),
+			},
 			want: want{
-				code: http.StatusTemporaryRedirect,
-				headers: []header{
-					{
-						name:  "Location",
-						value: testingURL,
-					},
-				},
+				code: http.StatusBadRequest,
 			},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			target := "/" + shortenedURL
-			res, _ := testRequest(t, ts, http.MethodGet, target, nil)
+			res, resBodyString := testRequest(t, ts, test.request)
 			res.Body.Close()
-			ts.Close()
+
+			assert.Equal(t, test.want.code, res.StatusCode)
+			assert.NotEmpty(t, resBodyString)
+			if test.want.contentType != "" {
+				assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
+			}
+		})
+	}
+}
+
+func TestRestoreURLHandler(t *testing.T) {
+	shortenedUrlsByID := app.ShortenedUrlsByIDType{
+		"some_id": testingURL,
+	}
+	for key, value := range shortenedUrlsByID {
+		app.ShortenedUrlsByID[key] = value
+	}
+
+	tests := []test{
+		{
+			name: "negative test #0",
+			request: request{
+				method: http.MethodGet,
+				path:   "/" + "not_existing_id",
+			},
+			want: want{
+				code: http.StatusBadRequest,
+			},
+		},
+	}
+	i := 0
+	for key, value := range shortenedUrlsByID {
+		tests = append(
+			tests,
+			test{
+				name: fmt.Sprintf("positive test %d", i),
+				request: request{
+					method: http.MethodGet,
+					path:   "/" + key,
+				},
+				want: want{
+					code: http.StatusTemporaryRedirect,
+					headers: []header{
+						{
+							name:  "Location",
+							value: value,
+						},
+					},
+				},
+			},
+		)
+		i++
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, _ := testRequest(t, ts, test.request)
+			res.Body.Close()
 
 			assert.Equal(t, test.want.code, res.StatusCode)
 			for _, header := range test.want.headers {
@@ -101,4 +154,5 @@ func TestRestoreURLHandler(t *testing.T) {
 			}
 		})
 	}
+	ts.Close()
 }
